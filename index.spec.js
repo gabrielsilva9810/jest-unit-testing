149f94c93af3c35bf9a47014252c148bb18c4ddf
Gabriel Silva <gabrielsilva9810@gmail.com>
2025-03-01 16:09:43 -0300

    Add another failing test

--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,20 @@
-const calculaComissaoDeVenda = requie ('.')
+const calculaComissaoDeVenda = require ('.')
 
 it('se a lista estiver vazia, a comissão é zero', () => {
     const resultadoAtual = calculaComissaoDeVenda([])
     const resultadoEsperado = 0
+
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
+
+it ('calcula comissão quando só tem um item na lista', () => {
+    const resultadoAtual = calculaComissaoDeVenda([{
+        id: 'PROD-123',
+        precoUnitario: 1000,
+        quantidadeVendida: 1
+    }])
+
+    const resultadoEsperado = 50
 
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
