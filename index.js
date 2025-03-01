988387b75904ebca311ef697d35a58bba2344bba
Gabriel Silva <gabrielsilva9810@gmail.com>
2025-03-01 17:07:00 -0300

    And a bit more

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = function (items) {
-    if (!items.length) return 0
     const totalPrice = items.map(item => {
         const { unitPrice, quantitySold } = item
         return unitPrice * quantitySold
-    }).reduce((a, b) => a + b)
-    
+    }).reduce((a, b) => a + b, 0)
+
     return totalPrice * .05
 }
