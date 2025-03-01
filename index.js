f804773f75838c25905732015da813bdf838843f
Gabriel Silva <gabrielsilva9810@gmail.com>
2025-03-01 16:57:50 -0300

    Refactor code a bit more

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.exports = function (items) {
     if (!items.length) return 0
-    const prices = items.map(item => {
+    const totalPrice = items.map(item => {
         const { unitPrice, quantitySold } = item
         return unitPrice * quantitySold
-    })
-
-    const totalPrice = prices.reduce((a, b) => a + b)
+    }).reduce((a, b) => a + b)
+    
     return totalPrice * .05
 }
