040d6ffabd857e8ccc9195eb4e9a23b49238f6f5
Gabriel Silva <gabrielsilva9810@gmail.com>
2025-03-01 16:32:36 -0300

    Add test to prove a missing feature

--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,6 @@ const calculaComissaoDeVenda = require ('.')
 it('se a lista estiver vazia, a comissão é zero', () => {
     const resultadoAtual = calculaComissaoDeVenda([])
     const resultadoEsperado = 0
-
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
 
@@ -15,6 +14,23 @@ it ('calcula comissão quando só tem um item na lista', () => {
     }])
 
     const resultadoEsperado = 50
+    expect(resultadoAtual).toBe(resultadoEsperado)
+})
 
+it ('calcula comissão quando há mais de um item na lista', () => {
+    const resultadoAtual = calculaComissaoDeVenda([
+        {
+            id: 'PROD-123',
+            precoUnitario: 1000,
+            quantidadeVendida: 1 
+        },
+        {
+            id: 'PROD-456',
+            precoUnitario: 100,
+            quantidadeVendida: 5 
+        }
+    ])
+
+    const resultadoEsperado = 75
     expect(resultadoAtual).toBe(resultadoEsperado)
 })
