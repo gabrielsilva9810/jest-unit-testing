0cd1d2bd23cf59f80bac352b10c8007879b548bb
Gabriel Silva <gabrielsilva9810@gmail.com>
2025-03-01 17:25:58 -0300

    Add special condition feature

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 module.exports = function (items) {
+    let foundSpecialProduct = false
     const totalPrice = items.map(item => {
-        const { unitPrice, quantitySold } = item
+        const { id, unitPrice, quantitySold } = item
+        if(id === 'XP-0101'){
+            foundSpecialProduct = true
+        }
         return unitPrice * quantitySold
     }).reduce((a, b) => a + b, 0)
 
+    if(foundSpecialProduct) return totalPrice * .2
     if(totalPrice < 1999.99) return totalPrice * .05
     if(totalPrice > 1999.99 && totalPrice < 4999.99) return totalPrice * .1
     if (totalPrice > 4999.99) return totalPrice * .15
